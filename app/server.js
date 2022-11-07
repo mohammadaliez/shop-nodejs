aeec24883ac23f44467996b73ea27473992ef2dc
ali <ali1995ez@gmail.com>
2022-11-07 16:00:07 +0330

    add eslint and prettier

--- app/server.js
+++ app/server.js
@@ -1,55 +1,55 @@
-const express = require("express");
-const { default: mongoose } = require("mongoose");
-const path = require("path");
+const express = require('express')
+const {default: mongoose} = require('mongoose')
+const path = require('path')
+const {AllRoutes} = require('./router/router')
 
-module.exports = class Application{
-    #app = express();
-    #DB_URI;
-    #PORT;
-    constructor(PORT,DB_URI){
-        this.#PORT = PORT;
-        this.#DB_URI = DB_URI;
-        this.configApplication();
-        this.connectToMongoDB();
-        this.createServer();
-        this.createRoutes();
-        this.errorHandling();
-    }
-    configApplication(){
-        this.#app.use(express.json())
-        this.#app.use(express.urlencoded({extended:true}))
-        this.#app.use(express.static(path.join(__dirname,"..","public")))
-    }
-    createServer(){
-        const http = require("http")
-        http.createServer(this.#app).listen(this.#PORT,()=>{
-            console.log("run > http://localhost:" + this.#PORT);
-        })
-    }
-    connectToMongoDB(){
-        mongoose.connect(this.#DB_URI,(error)=>{
-            if(!error) return console.log("connected to MongoDB");
-            return console.log("failed to connect to MongoDB");
-        })
-    }
-    createRoutes(){
-
-    }
-    errorHandling(){
-        this.#app.use((req,res,next)=>{
-            return res.status(404).json({
-                statusCode:404,
-                message:"Router not found"
-            })
-        })
-        this.#app.use((error,req,res,next)=>{
-            const statusCode = err.status || 500
-            const message = error.message || "Internal server error"
-            return res.status(statusCode).json({
-                statusCode,
-                message
-            })
-        })
-    }
-
+module.exports = class Application {
+  #app = express()
+  #DB_URI
+  #PORT
+  constructor(PORT, DB_URI) {
+    this.#PORT = PORT
+    this.#DB_URI = DB_URI
+    this.configApplication()
+    this.connectToMongoDB()
+    this.createServer()
+    this.createRoutes()
+    this.errorHandling()
+  }
+  configApplication() {
+    this.#app.use(express.json())
+    this.#app.use(express.urlencoded({extended: true}))
+    this.#app.use(express.static(path.join(__dirname, '..', 'public')))
+  }
+  createServer() {
+    const http = require('http')
+    http.createServer(this.#app).listen(this.#PORT, () => {
+      console.log('run > http://localhost:' + this.#PORT)
+    })
+  }
+  connectToMongoDB() {
+    mongoose.connect(this.#DB_URI, error => {
+      if (!error) return console.log('connected to MongoDB')
+      return console.log('failed to connect to MongoDB')
+    })
+  }
+  createRoutes() {
+    this.#app.use(AllRoutes)
+  }
+  errorHandling() {
+    this.#app.use((req, res, next) => {
+      return res.status(404).json({
+        statusCode: 404,
+        message: 'Route not found',
+      })
+    })
+    this.#app.use((error, req, res, next) => {
+      const statusCode = error.status || 500
+      const message = error.message || 'Internal server error'
+      return res.status(statusCode).json({
+        statusCode,
+        message,
+      })
+    })
+  }
 }
--- index.js
+++ index.js
@@ -1,2 +1,2 @@
-const Application = require("./app/server")
-new Application(5000,"mongodb://localhost:27017/storeDB")
+const Application = require('./app/server')
+new Application(5000, 'mongodb://localhost:27017/storeDB')
